22df3550e140ef2300cfcc46bbf04461a8648c77
EL Mehdi SOUMMER <63970317+Strifee@users.noreply.github.com>
2021-09-10 02:37:06 +0100

    Update hyper_parameters.py

--- hyper_parameters.py
+++ hyper_parameters.py
@@ -4,15 +4,15 @@ from train import BATCH_SIZE
 class Hyperparam:
     """Hyper parameters"""
     # Training
-    BATCH_SIZE = 32
-    learning_rate = 0.0002
-    num_epochs = 50
+    BATCH_SIZE = 16
+    learning_rate = 0.0001
+    num_epochs = 30
 
     # Model
     num_heads = 8
     num_encoder_layers = 3
     num_decoder_layers = 3
 
-    max_len= 50
+    max_len= 230
     dropout = 0.4
     embedding_size= 256
